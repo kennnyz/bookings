b61db968ad8252fdf036de13fe2bbf5b590619aa
Muhammed Muhtarov <muhammed1410@yandex.ru>
2023-01-01 22:49:34 +0300

    AvailabilityJSon

--- pckg/handler/handlers.go
+++ pckg/handler/handlers.go
@@ -1,10 +1,12 @@
 package handlers
 
 import (
+	"encoding/json"
 	"fmt"
 	"github.com/kennnyz/bookings/pckg/config"
 	"github.com/kennnyz/bookings/pckg/models"
 	"github.com/kennnyz/bookings/pckg/render"
+	"log"
 	"net/http"
 )
 
@@ -77,6 +79,25 @@ func (m *Repository) PostAvailability(w http.ResponseWriter, r *http.Request) {
 	start := r.Form.Get("start")
 	end := r.Form.Get("end")
 	w.Write([]byte(fmt.Sprintf("Start date is %s, end date is %s", start, end)))
+}
+
+type jsonResponse struct {
+	OK      bool   `json:"ok"`
+	Message string `json:"message"`
+}
+
+// AvailabilityJSON handles request for availability and send JSON response
+func (m *Repository) AvailabilityJSON(w http.ResponseWriter, r *http.Request) {
+	resp := jsonResponse{
+		OK:      true,
+		Message: "Available",
+	}
+	out, err := json.MarshalIndent(resp, "", "     ")
+	if err != nil {
+		log.Println(err)
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(out)
 }
 
 // Contact renders the contact page
